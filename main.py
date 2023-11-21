c93590b50d01f6f3f1305539cbf18da99a3923de
a-dugonin <a_dugonin@mail.ru>
2023-11-21 14:06:51 +0300

    version 6

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from new_family import start
 
-start().print_info()
+parent = start()
+parent.child_conditions_all()
+parent.calm_down_child('Darina')
+parent.calm_down_child('Sava')
+parent.calm_down_child('Sava')
+
--- new_family.py
+++ new_family.py
@@ -1,79 +1,87 @@
 def start():
+
+    HUNGER_LEVELS = {0: 'голоден', 1: 'не голоден'}
+    CALM_LEVELS = {0: 'раздражен', 1: 'слегка капризничает', 2: 'спокоен'}
+
     class Parent:
         """Данный класс создает объект 'Родитель'"""
-        def __init__(self, name: str, age: int, children: list):
+        def __init__(self, name: str, age: int, children: dict):
+            """Конструктор класса Parent"""
             self.name = name
             self.age = age
             self.children = children
 
-        def print_info(self):
-            """Данный метод возвращает основную информацию о родителе"""
-            print(f'Меня зовут {self.name}, мне {self.age} лет и у меня {len(self.children)} детей')
+        def get_info(self):
+            """Геттер, возвращает основную информацию о родителе"""
+            return f'Меня зовут {self.name}, мне {self.age} лет и у меня {len(children)} детей'
 
-        def child_conditions_all(self):
+        def child_conditions_all(self, name_child=''):
             """Метод позволяет вывести информацию о состоянии детей родителя"""
-            for child in self.children:
-                child.print_info_state_hunger()
-                child.print_info_state_calm()
+            if not name_child:
+                for child in self.children:
+                    print(f'{child} {HUNGER_LEVELS[children[child].state_hunger]} и {CALM_LEVELS[children[child].state_calm]}')
+            else:
+                print(f'{name_child} {HUNGER_LEVELS[children[name_child].state_hunger]} и {CALM_LEVELS[children[name_child].state_calm]}')
 
-        def calm_down_child(self):
-            """Метод позволяет успокоить ребенка"""
-            for child in self.children:
-                child.calm_down()
+        def calm_down_child(self, name_child=''):
+            """Метод позволяет успокоить ребенка при указании его имени или сразу всех детей"""
+            if not name_child:
+                for child in self.children:
+                    if children[child].state_calm < 2:
+                        self.children[child].change_state_calm()
+                        print(f'Успокаиваем {child}. {child} сейчас {CALM_LEVELS[children[child].state_calm]}')
+                    else:
+                        print(f'{child} спокоен')
+            else:
+                if children[name_child].state_calm < 2:
+                    self.children[name_child].change_state_calm()
+                    print(f'Успокаиваем {name_child}. {name_child} сейчас {CALM_LEVELS[children[name_child].state_calm]}')
+                else:
+                    print(f'{name_child} спокоен')
 
-        def feed_child(self):
-            """Метод позволяет покормить ребенка"""
-            for child in self.children:
-                child.eats()
+        def feed_child(self, name_child=''):
+            """Метод позволяет покормить ребенка при указании его имени или сразу всех детей"""
+            if not name_child:
+                for child in self.children:
+                    if not children[child].state_hunger:
+                        self.children[child].change_state_hunger()
+                        print(f'{child} покормлен')
+                    else:
+                        print(f'{child} не голоден')
+            else:
+                if not children[name_child].state_hunger:
+                    self.children[name_child].change_state_hunger()
+                    print(f'{name_child} покормлен')
+                else:
+                    print(f'{name_child} не голоден')
 
     class Child:
         """Данный класс создает объект 'Ребенок'"""
-        HUNGER_LEVELS = {0: 'голоден', 1: 'не голоден'}
-        CALM_LEVELS = {0: 'раздражен', 1: 'слегка капризничает', 2: 'спокоен'}
 
         def __init__(self, name_child, age_child, state_hunger=0, state_calm=0):
+            """Конструктор класса Child"""
             self.name_child = name_child
             self.age_child = age_child
             self.state_hunger = state_hunger
             self.state_calm = state_calm
 
-        def eats(self):
+        def change_state_hunger(self):
             """Метод позволяет изменить состояние голода ребенка"""
-            if not self.state_hunger:
-                self.state_hunger += 1
-                print(f'{self.name_child} покормлен')
-            else:
-                self.print_info_state_hunger()
+            self.state_hunger += 1
 
-        def calm_down(self):
+        def change_state_calm(self):
             """Метод позволяет изменить состояние спокойствия ребенка"""
-            if self.state_calm < 2:
-                self.state_calm += 1
-                print(f'Успокаиваем {self.name_child}')
-                if self.state_calm < 2:
-                    print(f'{self.name_child} немного успокоился, но еще все равно не в лучшем настроении!')
-                else:
-                    self.print_info_state_calm()
-            else:
-                self.print_info_state_calm()
-
-        def print_info_state_hunger(self):
-            """Метод выводит информацию о состоянии голода ребенка"""
-            print(f'{self.name_child} сейчас {self.HUNGER_LEVELS[self.state_hunger]}')
-
-        def print_info_state_calm(self):
-            """Метод выводит информацию о состоянии спокойствия ребенка"""
-            print(f'{self.name_child} сейчас {self.CALM_LEVELS[self.state_calm]}')
+            self.state_calm += 1
 
     name_parrent, age_parrent = 'Anton', 35
     name_child, age_child, state_hunger, state_calm = ['Darina', 'Sava'], [18, 3], [1, 0], [1, 0]
     count_child = 2
-    children = []
+    children = {}
     for index in range(count_child):
         if age_child[index] >= age_parrent - 16:
             print('Ребенок должен быть младше родителя на 16 лет!')
         else:
-            children.append(Child(name_child[index], age_child[index], state_hunger[index], state_calm[index]))
+            children[name_child[index]] = Child(name_child[index], age_child[index], state_hunger[index], state_calm[index])
     parent = Parent(name_parrent, age_parrent, children)
     return parent
 
